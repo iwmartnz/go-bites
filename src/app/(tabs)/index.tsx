643f9b4d957c695748a1e3b973bc511f8bb0c5fc
iwmartnz <iwmartnz@gmail.com>
2024-09-19 13:02:43 -0600

    Added a redirect to the menu screen

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,36 +1,6 @@
-import { StyleSheet } from 'react-native';
+import { Redirect } from 'expo-router';
+import React from 'react';
 
-import { Text, View } from '@/components/ui';
-import Product from '@/components/product';
-import products from '../../../assets/data/products';
-
-const product = products[0];
-
-export default function TabOneScreen() {
-  return (
-    <View style={styles.container}>
-      <Product
-        image={product.image}
-        name={product.name}
-        price={product.price}
-      />
-    </View>
-  );
+export default function Tabindex() {
+  return <Redirect href={`/menu/`} />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
