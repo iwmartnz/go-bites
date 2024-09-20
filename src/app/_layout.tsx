e92d6d101ce552bbf51703e2c7130316cca1eb49
iwmartnz <iwmartnz@gmail.com>
2024-09-19 20:19:13 -0600

    Updated routes to distinguish between user and admin access

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -61,7 +61,8 @@ function RootLayoutNav() {
         <Stack
           screenOptions={{ headerStyle: { backgroundColor: theme.background } }}
         >
-          <Stack.Screen name='(tabs)' options={{ headerShown: false }} />
+          <Stack.Screen name='(admin)' options={{ headerShown: false }} />
+          <Stack.Screen name='(user)' options={{ headerShown: false }} />
           <Stack.Screen
             name='cart'
             options={{
