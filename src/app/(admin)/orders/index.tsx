c42c099e696e4e83a56ef7e11d7029700b50ab1f
iwmartnz <iwmartnz@gmail.com>
2024-09-26 09:10:09 -0600

    Added segmented control component.

--- src/app/(admin)/orders/index.tsx
+++ src/app/(admin)/orders/index.tsx
@@ -2,12 +2,34 @@ import { Stack } from 'expo-router';
 import { FlatList, StyleSheet } from 'react-native';
 
 import OrderListItem from '@/components/order-list-item';
+import SegmentedControl from '@react-native-segmented-control/segmented-control';
 
 import { useTheme } from '@/hooks/useTheme';
 import orders from '../../../../assets/data/orders';
+import { useEffect, useState } from 'react';
+import { Order } from '@/types';
+import { Text } from '@/components/ui';
 
 export default function OrdersPage() {
   const { theme } = useTheme();
+  const [index, setIndex] = useState(0);
+  const [ordersList, setOrdersList] = useState<Order[]>(orders);
+
+  useEffect(() => {
+    if (index === 0) {
+      setOrdersList(orders);
+    } else if (index === 1) {
+      const deliveredOrders = orders.filter(
+        (item) => item.status === 'Cooking'
+      );
+      setOrdersList(deliveredOrders);
+    } else if (index === 2) {
+      const deliveredOrders = orders.filter(
+        (item) => item.status === 'Delivered'
+      );
+      setOrdersList(deliveredOrders);
+    }
+  }, [index]);
 
   return (
     <>
@@ -16,12 +38,25 @@ export default function OrdersPage() {
           title: 'Orders',
           headerLargeTitle: true,
           headerLargeTitleStyle: { fontFamily: 'Satoshi-Black' },
-          headerSearchBarOptions: { autoCapitalize: 'none' },
+          headerSearchBarOptions: {
+            autoCapitalize: 'none',
+            barTintColor: '#fff',
+          },
+          headerTitle: (props) => (
+            <SegmentedControl
+              values={['All', 'Archive']}
+              style={{ height: 30, width: '70%' }}
+              selectedIndex={index}
+              onChange={(event) => {
+                setIndex(event.nativeEvent.selectedSegmentIndex);
+              }}
+            />
+          ),
         }}
       />
       <FlatList
         contentInsetAdjustmentBehavior='automatic'
-        data={orders}
+        data={ordersList}
         renderItem={({ item }) => <OrderListItem order={item} />}
         style={[{ backgroundColor: theme.background }, styles.container]}
         contentContainerStyle={styles.content}
